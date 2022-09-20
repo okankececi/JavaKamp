2cfbe050811255bafa0c773276b8b3cd3046fb0f
okankececi <34377924+okankececi@users.noreply.github.com>
2022-09-21 02:18:32 +0300

    .

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogreciSayisi = 12;
+		String mesaj = "Öğrenci sayısı :";
+		
+		System.out.println(mesaj + ogreciSayisi);
+		System.out.println(mesaj + ogreciSayisi);
+		System.out.println("Öğrenci sayım:" + ogreciSayisi);
+		System.out.println("Öğrenci sayım:" + ogreciSayisi);
+		System.out.println("Öğrenci sayım:" + ogreciSayisi);
+		
+		
+		
+
+	}
+
+}
